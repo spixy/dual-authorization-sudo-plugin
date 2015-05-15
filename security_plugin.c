153afb680211962efe8749bff4746d61429dd2f8
Spixy16@gmail.com <Spixy16@gmail.com@0ea1abd4-c7fb-1455-56e1-f8ba1e3c0803>
2015-05-15 23:15:55 +0000

    added license info

--- security_plugin.c
+++ security_plugin.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2015 Horváth Henrich
+ *
+ * Sudo security plugin is free software
+ * released under GNU Lesser General Public License.
+ *
+*/
+
 #define _GNU_SOURCE
 
 #include <ctype.h>
--- sudo_helper.h
+++ sudo_helper.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2015 Horváth Henrich
+ *
+ * Sudo security plugin is free software
+ * released under GNU Lesser General Public License.
+ *
+*/
+
 #ifndef SUDO_HELPER_INCLUDED
 #define SUDO_HELPER_INCLUDED
 
